641bc205a20aec9a7f3f9978bbcd126240005bc1
Steve Mokris <steve@kosada.com>
2019-10-08 17:31:56 -0400

    Update to GetWindowID 1.1

--- getwindowid.rb
+++ getwindowid.rb
@@ -1,8 +1,8 @@
 class Getwindowid < Formula
   desc "Retrieves the CGWindowID of the specified window"
   homepage "https://github.com/smokris/GetWindowID"
-  url "https://github.com/smokris/GetWindowID/archive/1.0.tar.gz"
-  sha256 "7f4ef6a215f52e76b0dae6fefb34ee7f83ae44daef6c5d2d55f9f8614fbb28ab"
+  url "https://github.com/smokris/GetWindowID/archive/1.1.tar.gz"
+  sha256 "fa6f1c28c675041021c8fe7944446e6d98f764140ac3a62a2707a220bd116c04"
 
   def install
     system "make"
