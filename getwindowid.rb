22fc6f34c85603be4c137417dd55714c1ebd05a3
Babil G. Sarwar <gsbabil@gmail.com>
2023-03-11 02:03:18 +1100

    Update GetWindowID to 1.1.2

--- getwindowid.rb
+++ getwindowid.rb
@@ -1,8 +1,8 @@
 class Getwindowid < Formula
   desc "Retrieves the CGWindowID of the specified window"
   homepage "https://github.com/gsbabil/GetWindowID"
-  url "https://github.com/gsbabil/GetWindowID/archive/1.1.1.tar.gz"
-  sha256 "802401935fec24231143b4bbcf1ccafe36a1f9eec8e0eb900afe52144f009032"
+  url "https://github.com/gsbabil/GetWindowID/archive/1.1.2.tar.gz"
+  sha256 "79d2cc171a6686a2fcd98d556876124fb2782b3c148a51aecddb85967f71fb46"
 
   def install
     system "make"
